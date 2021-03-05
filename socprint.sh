f026d5f3a18736d8debfeff3407d478649f213f1
Donald Lee <dlqs@gmx.com>
2021-03-05 23:56:29 +0800

    YOLO
    locals are not posix
    
    set posix
    
    exit posix
    
    remove non posix [[ ]]
    
    posixify
    
    reformat
    
    posix
    
    posix
    
    bug
    
    yolo
    
    asdasd

--- socprint.sh
+++ socprint.sh
@@ -1,122 +1,149 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-set -Eeuo pipefail
-trap cleanup SIGINT SIGTERM ERR EXIT
-
-cleanup() {
-  trap - SIGINT SIGTERM ERR EXIT
-  # script cleanup here
-}
-
-export LC_ALL=C
+set -euf
 
 host="sunfire.comp.nus.edu.sg"
 default_printqueue="psc008-dx"
+default_script="/usr/local/bin/socprint.sh"
 
 usage() {
   cat <<EOF
-Bash script to print stuff in NUS SoC.
+NAME
+  socprint - POSIX™-compliant, zero-dependency shell script to print stuff in NUS SoC
 
-Requirements: bash, a sunfire account, and connection to SoC wifi.
+REQUIREMENTS
+  POSIX™-compliant sh, a sunfire account, and connection to SoC wifi.
 
-Usage (copy-and-paste this one line):
- curl -s https://raw.githubusercontent.com/dlqs/SOCprint/master/socprint.sh | bash -s -- -u <username> -f <filename> -p <printqueue>
+USAGE
+  To use instantly, run the following line:
+  curl -s https://raw.githubusercontent.com/dlqs/SOCprint/master/socprint.sh | sh -s -- -u <username> -f <filepath> -p <printqueue>
 
-Usage (download and run from any directory):
- sudo curl https://raw.githubusercontent.com/dlqs/SOCprint/master/socprint.sh -o /usr/local/bin/socprint.sh
- sudo chmod 755 /usr/local/bin/socprint.sh
- socprint.sh -u <username> -f <filename> -p <printqueue>
+  To download and run from any directory:
+  sudo curl https://raw.githubusercontent.com/dlqs/SOCprint/master/socprint.sh -o $default_script
+  sudo chmod 755 $default_script
+  socprint.sh -u <username> -f <filepath> -p <printqueue>
 
-Parameters:
- -u, --username         (required) Sunfire username (without the @sunfire.comp.nus.edu.sg part).
- -i, --identity-file    (optional) Additional identity file to use with ssh. Skip if you already set up sunfire identity files for ssh.
- -f, --filename         (required to print) File to print. Tested with PDF/plain text files. Undefined behaviour for anything else.
- -p, --printqueue       (optional to print) Printqueue to send job to. Defaults to psc008-dx.
- -l, --list-printqueues (required to list printqueues) List printqueues i.e. valid arguments for -p.
- --dry-run              (for debugging/tests) echoes the commands to be executed without executing them 
+PARAMETERS
+  -u, --username <username>
+    (required) Sunfire username, without the @sunfire.comp.nus.edu.sg part.
 
-Print command example:
- ./socprint.sh -u d-lee -f ~/Downloads/cs3210_tutorial8.pdf -p psc008-dx
+  -i, --identity-file <filepath>
+    (optional) Additional identity file to use with ssh. Skip if you already set up sunfire identity files for ssh.
 
-List printqueue command example:
- ./socprint.sh -u d-lee -l
+  -f, --filepath <filepath>
+    (required to print) File to print. Tested with PDF/plain text files. Undefined behaviour for anything else.
 
-Roughly speaking, this script will:
- 1. Login to sunfire using ssh.
-    You will be prompted for your password, unless your sunfire identity files are set up.
-    This script *does not* save/record your password.
- 2. Copy your file into your home directory in sunfire.comp.nus.edu.sg to a temporary, random name.
- 3. Submit your job to the printqueue.
- 4. List the printqueue. You *should* see your job here. If not, something has gone wrong.
- 5. Remove the temporary file.
+  -p, --printqueue <printqueue>
+    (required to print/show) Printqueue to send job to. Default: psc008-dx. See PRINTQUEUES.
 
-Printqueues:
- - (you're probably looking for these locations)
- - COM1 basement:                   psc008 psc008-dx psc008-sx psc011 psc011-dx psc011-sx
- - COM1 L1, in front of tech svsc:  psts psts-dx psts-sx pstsb pstsb-dx pstsb-sx
- - (no suffix) or -dx: double sided
- - -sx: single sided
- - Most other printers have user restrictions. See https://dochub.comp.nus.edu.sg/cf/guides/printing/print-queues
- - For the full list of printqueues, generate with the -l option, or view README at the source.
+  -s, --show-printqueue
+    (required to show) Show list of jobs at specified printqueue.
 
-Source and README: https://github.com/dlqs/SOCprint
+  -l, --list-printqueues
+    (required to list printqueues) List printqueues, i.e. valid arguments for -p.
 
-Contributors: Donald Lee, Julius Nugroho, Sean Ng
+  --dry-run
+    (for debugging/tests) Echoes commands to be executed without executing them.
 
-README command (prints this help + list of valid printqueues):
-./socprint.sh -h > README \
- && echo "List of valid printqueues, generated with -l option on 5 March 2021\n" >> README \
- && ./socprint.sh -u d-lee -l >> README
+EXAMPLES
+  To print:
+  ./socprint.sh -u d-lee -f ~/Downloads/cs3210_tutorial8.pdf -p psc008-dx
+
+  To list printqueues:
+  ./socprint.sh -u d-lee -l
+
+  To show printqueue at psc008:
+  ./socprint.sh -u d-lee -s -p psc008
+
+DESCRIPTION
+  This script targets conformance to POSIX.1-2017 standards (https://pubs.opengroup.org/onlinepubs/9699919799/).
+  This improves portability, enterprise-gradeability, and makes printing in SoC a zero-dependency,
+  no-ass-sucking experience. Roughly speaking, this script will:
+  1. Login to sunfire using ssh.
+     You will be prompted for your password, unless your identity files are set up.
+     This script *does not* save/record your password.
+  2. Copy the file into your home directory in sunfire, to a temporary file.
+  3. Submit your job to the printqueue.
+  4. List the printqueue. You job *should* appear. If not, something has gone wrong.
+  5. Remove the temporary file.
+
+PRINTQUEUES
+  Popular places:
+  - COM1 basement:                   psc008 psc008-dx psc008-sx psc011 psc011-dx psc011-sx
+  - COM1 L1, in front of tech svsc:  psts psts-dx psts-sx pstb pstb-dx pstb-sx
+  - (no suffix)/-dx: double sided, -sx: single sided, -nb: no banner
+  - Most other printers have user restrictions.
+    See https://dochub.comp.nus.edu.sg/cf/guides/printing/print-queues.
+  - For the full list of printqueues, generate with the -l option, or view the SOURCE.
+
+SOURCE
+  https://github.com/dlqs/SOCprint
+  File bugs or compliance issues above. We take POSIX™ conformance seriously.
+  POSIX™ is a Trademark of The IEEE.
+
+CONTRIBUTORS
+  Donald Lee, Julius Nugroho, Sean Ng
+
+GENERATE README
+  ./socprint.sh -h > README \
+  && echo "List of valid printqueues, generated with -l option on 5 March 2021\n" >> README \
+  && ./socprint.sh -u d-lee -l >> README
 
 EOF
-  exit
+  exit 0
 }
 
 msg() {
-  echo >&2 -e "${1-}"
+  # Log messages to stderr instead of stdout
+  # Woahhhhh %b siol
+  printf "%b\n" "${1-}" >&2
 }
 
 die() {
-  local msg=$1
-  local code=${2-1} # default exit status 1
-  msg "$msg"
-  exit "$code"
+  msg "$1"
+  exit 1
 }
 
 check_updates() {
-  # Calculate git hash-object hash without git
-  local my_sha=$((perl -e '$size = (-s shift); print "blob $size\x00"' /usr/local/bin/socprint.sh && cat /usr/local/bin/socprint.sh) | shasum -a 1 | cut -f 1 -d ' ')
+  # Calculate git hash-object hash without git, since it is not POSIX compliant
+  size=$( wc -c ${default_script} | cut -f 1 -d ' ' )
+  my_sha=$( (printf "blob %s\0" "$size" && cat ${default_script}) | shasum -a 1 )
+
   # Pull latest hash from master
-  local github_sha=$(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/dlqs/SOCprint/contents/socprint.sh | sed -n 's/.*"sha":\s"\(.*\)",/\1/p')
-  if [[ $my_sha != $github_sha ]]; then
-    msg "Hint: You appear to have downloaded this script to /usr/local/bin/socprint.sh. There's a newer version available (${my_sha:0:10}) v (${github_sha:0:10})."
+  github_sha=$( curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/dlqs/SOCprint/contents/socprint.sh | sed -n 's/.*"sha":\s"\(.*\)",/\1/p' )
+  if [ "$my_sha" != "$github_sha" ]; then
+    msg "Hint: You appear to have downloaded this script to $default_script. There's a newer version available ($( printf '%s' "$my_sha" | head -c 10) v $( printf '%s' "$github_sha" | head -c 10 ))."
     msg "Run the following command to download the new script:"
-    msg "sudo curl https://raw.githubusercontent.com/dlqs/SOCprint/master/socprint.sh -o /usr/local/bin/socprint.sh\n"
+    msg "sudo curl https://raw.githubusercontent.com/dlqs/SOCprint/master/socprint.sh -o $default_script \n"
   fi
 }
 
 parse_params() {
-  # default values of variables set from params
+  # default values of variables
   identity_file=''
   printqueue=''
   list_printqueues=false
+  show_printqueue=false
 
   while :; do
     case "${1-}" in
     -h | --help) usage ;;
-    -l | --list-printqueues) 
+    -l | --list-printqueues)
       list_printqueues=true
       ;;
-    -i | --identity-file) 
-      identity_file="${2-}" 
+    -s | --show-printqueue)
+      show_printqueue=true
+      ;;
+    -i | --identity-file)
+      identity_file="${2-}"
       shift
       ;;
-    -p | --printqueue) 
-      printqueue="${2-}" 
+    -p | --printqueue)
+      printqueue="${2-}"
       shift
       ;;
-    -f | --filename) 
-      filename="${2-}" 
+    -f | --filepath)
+      filepath="${2-}"
       shift
       ;;
     -u | --username)
@@ -139,60 +166,64 @@ parse_params "$@"
 
 # Only check update if downloaded to local bin and we're not dry-running, as
 # this could potentially mess tests up.
-[[ -f "/usr/local/bin/socprint.sh" && -z "${dry_run-}" ]] && check_updates
+[ -f "/usr/local/bin/socprint.sh" ] && [ -z "${dry_run-}" ] && check_updates
 
-[[ -z "${username-}" ]] && die "Missing required parameter: -u/--username"
+[ -z "${username}" ] && die "Missing required parameter: -u/--username"
 sshcmd="${username}@${host}"
-if [[ ! -z ${identity_file} ]]; then
-  # Use the ssh identity_file if provided
-  sshcmd="${sshcmd} -i ${identity_file}"
-fi
 
+# Use the ssh identity_file if provided
+[ -n "${identity_file}" ] && sshcmd="${sshcmd} -i ${identity_file}"
 
-if [[ ! -z "${dry_run-}" ]];
-then
-  eval_or_echo_in_dry_run="echo"
+if [ -n "${dry_run-}" ]; then
+  eval_or_echo_in_dry_run='echo'
 else
-  eval_or_echo_in_dry_run="eval"
+  eval_or_echo_in_dry_run='eval'
 fi
 
 msg "Using ${username}@${host} ..."
 
-if [[ $list_printqueues == true ]]; then
-  # Thanks @pengnam for the regex
-  cmd=$(cat <<EOF
+if [ "${list_printqueues}" = true ]; then
+  cmd=$( cat <<EOF
 ssh $sshcmd "cat /etc/printcap | grep '^p' | sed 's/^\([^:]*\).*$/\1/'"
 EOF
   )
-  $eval_or_echo_in_dry_run $cmd
+  $eval_or_echo_in_dry_run "$cmd"
   exit 0
 fi
 
-[[ -z "${filename-}" ]] && die "Missing required parameter: -f/--filename"
-[[ ! -f "${filename-}" ]] && die "Error: No such file"
-
-filetype=$(file -i $filename | cut -f 2 -d ' ')
-[[ $filetype != "application/pdf;" && $filetype != text* ]] && msg "Warning: File is not PDF or text. Print behaviour is undefined."
-
-# Generate a random 8 character alphanumeric string *without* tr
-tempname=$(perl -e '@c=("A".."Z","a".."z",0..9);$p.=$c[rand(scalar @c)] for 1..8; print "$p\n"')
-tempname="SOCPrint_${tempname}"
-
-if [[ -z "${printqueue-}" ]];
-then
-  msg "Using default printqueue: ${default_printqueue}";
-  msg "Hint: To set a different one, use the -p option. To list all, use the -l option.";
-  printqueue=$default_printqueue;
+if [ -z "${printqueue-}" ]; then
+  msg "Using default printqueue: ${default_printqueue}"
+  msg "Hint: To set a different one, use the -p option. To list all, use the -l option."
+  printqueue="${default_printqueue}"
 fi
 
-cmd=$(cat <<EOF
+if [ "${show_printqueue}" = true ]; then
+  cmd=$( cat <<EOF
+ssh $sshcmd "lpq -P ${printqueue};"
+EOF
+  )
+  $eval_or_echo_in_dry_run "$cmd"
+  exit 0
+fi
+
+[ -z "${filepath-}" ] && die "Missing required parameter: -f/--filepath"
+[ ! -f "${filepath-}" ] && die "Error: No such file"
+
+filetype=$( file -i "${filepath}" | cut -f 2 -d ' ')
+[ "${filetype}" != 'application/pdf;' ] && [ "$( printf '%s' "$filetype" | head -c 4 )" != 'text' ] && msg "Warning: File is not PDF or text. Print behaviour is undefined."
+
+# Generate random 8 character alphanumeric string in a POSIX compliant way
+tempname=$( awk 'BEGIN{srand();for(i=0;i<8;i++){r=int(61*rand());printf("%c",r<10?48+r:r<35?55+r:62+r)}}' )
+tempname="SOCPrint_${tempname}"
+
+cmd=$( cat <<EOF
 ssh $sshcmd "
   cat - > ${tempname};
   lpr -P ${printqueue} ${tempname};
   lpq -P ${printqueue};
-  rm ${tempname};" < ${filename}
+  rm ${tempname};" < "${filepath}"
 EOF
   )
-$eval_or_echo_in_dry_run $cmd
+$eval_or_echo_in_dry_run "$cmd"
 
 exit 0
